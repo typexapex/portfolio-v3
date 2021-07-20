3200aeb6926f210d86e3371d877f1e0c6f1bdc5c
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-07-20 12:42:50 +0900

    project data added into object

---
+++ projects.js
@@ -0,0 +1,20 @@
+// object to hold all the project data
+const projectList = {
+  splitIt : {
+    "name" : "Split It",
+    "description" : "Split it is a mobile-first progressive web app that I helped create during my time at Le Wagon Tokyo. My team and I coded the entire project over a period of 2 weeks in June 2021, and I pitched it at Le Wagon's Pitch Night on Friday, June 11 to a crowd of ~80 on Zoom. My main contribution was on the front-end, although I had a hand in almost every area.",
+    "tech" : "<i class="devicon-rails-plain"></i><i class="devicon-ruby-plain"></i><i class="devicon-javascript-plain"></i><i class="devicon-html5-plain"></i><i class="devicon-sass-original"></i><i class="devicon-heroku-original"></i><i class="devicon-trello-plain"></i><i class="devicon-figma-plain"></i><i class="devicon-postgresql-plain"></i><i class="devicon-github-original"></i><i class="devicon-git-plain"></i>",
+    "image" : "images/split-it-group_iphone12black_portrait.png",
+    "site" : "http://www.split-it.me",
+    "code" : "https://github.com/typexapex/split-it"
+  },
+  chefSpace : {
+    "name" : "Chef Space",
+    "description" : "Chef Space is an AirBnb clone website that I helped create during my time studying at Le Wagon in Tokyo. My team and I coded the entire project over a period of 5 days in May 2021. For this project, I was the project manager and also contributed to all parts of the project.",
+    "tech" : "<i class="devicon-rails-plain"></i><i class="devicon-ruby-plain"></i><i class="devicon-javascript-plain"></i><i class="devicon-html5-plain"></i><i class="devicon-sass-original"></i><i class="devicon-heroku-original"></i> <i class="devicon-trello-plain"></i><i class="devicon-figma-plain"></i><i class="devicon-postgresql-plain"></i><i class="devicon-github-original"></i><i class="devicon-git-plain"></i>",
+    "image" : "images/chef-space-map.png",
+    "site" : "http://chefspcae.herokuapp.com/",
+    "code" : "https://github.com/typexapex/bill-bnb"
+  };
+};
+
