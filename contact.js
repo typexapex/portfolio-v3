c3db3956cd271299022268ebeea9cad3472746cb
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-07-21 16:55:06 +0900

    contact styled

--- contact.js
+++ contact.js
@@ -11,6 +11,7 @@ function fetchContactText(event) {
      `
       <div class="contact-text-card">
         <h2 class="contact-header">Contact Me!</h2>
+        <div class="line"></div>
         <p class="contact-p1">Looking for a new teammate?</p>
         <p class="contact-p2">Or maybe just a coffee?</p>
         <p class="contact-p3">Drop me a line! I'm always happy to chat!</p>
@@ -24,7 +25,6 @@ function fetchContactForm(event) {
   const contactForm =
     `
     <div class="contact-form-card">
-      <h2>Let's Connect!</h2>
       <form action="https://formspree.io/f/xpzkjdro" method="POST">
         <input type="email" name="_replyto" placeholder="your email here" id="email-address"><br><br>
         <textarea name="message" placeholder="your message here" id="email-message"></textarea><br><br>
